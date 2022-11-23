34e5674c54a032033b6b28becf59c82080de33a1
Zaid_ZiD <zid6177@gmail.com>
2022-11-23 17:40:14 +0500

    changes

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('response from zaid laptop', req.ip)
